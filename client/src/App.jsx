f0677051056d53ddb623fe0982310e625107b98d
Tristan <tristanshawnjohnson@gmail.com>
2021-04-23 13:55:51 -0700

    Removed component from boilerplate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,48 +10,13 @@ class App extends React.Component {
 
     }
     //bindings
-    this.onChange = this.onChange.bind(this);
-  }
 
-  //Example Pattern
-
-  // onChange(e) {
-  //   this.setState({
-  //     input1: e.target.value,
-  //   });
-  // }
-
-  //Generalization Problem
-
-  // onChange(e) {
-  //   if (e.target.name === 'input1') {
-  //     this.setState({
-  //       input1: e.target.value,
-  //     });
-  //   } else if (e.target.name === 'input2') {
-  //     this.setState({
-  //       input2: e.target.value,
-  //     });
-  //   } else if (e.target.name === 'input3') {
-  //     this.setState({
-  //       input3: e.target.value,
-  //     });
-  //   }
-  // }
-
-
-  //Solution
-
-  onChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
   }
 
 
   render () {
     return (
-      <div><Apattern state={this.state} onChange={this.onChange}/></div>
+      <div>React is Working</div>
     )
   }
 
