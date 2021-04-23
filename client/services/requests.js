80e6d4cb73af9effbe77534ae8f913ffb6dbe24b
Tristan <tristanshawnjohnson@gmail.com>
2021-04-23 15:39:44 -0700

    Implemented get request to obtain the routes for Heavy and Light Rails

--- client/services/requests.js
+++ client/services/requests.js
@@ -1,10 +1,27 @@
+const axios = require('axios');
+
+const getRoutes = function(callback) {
+
+  var axios = require('axios');
+
+  var config = {
+    method: 'get',
+    url: 'https://api-v3.mbta.com/routes?sort=type&filter%5Btype%5D=0,1',
+    headers: { }
+  };
+
+  axios(config)
+  .then(function (response) {
+    callback(JSON.stringify(response.data));
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+}
+
 module.exports = {
 
-  test() {
-    console.log('Hello from example services');
-  }
-
-  //all services code for example lives here
-  //I would put get and post requests here any request to the server
+  getRoutes
 
 }
