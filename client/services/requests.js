8d33cdd41233c675270da010d05e5c3ae900eb90
Tristan <tristanshawnjohnson@gmail.com>
2021-04-24 10:28:57 -0700

    Implemented a getPrediction request

--- client/services/requests.js
+++ client/services/requests.js
@@ -17,10 +17,10 @@ const getRoutes = function(callback) {
 
 }
 
-const getStops = function(routeID, callback) {
+const getStops = function(routeId, callback) {
   const config = {
     method: 'get',
-    url: `https://api-v3.mbta.com/stops?filter%5Broute%5D=${routeID}`,
+    url: `https://api-v3.mbta.com/stops?filter%5Broute%5D=${routeId}`,
     headers: { }
   };
 
@@ -32,6 +32,28 @@ const getStops = function(routeID, callback) {
     console.log(error);
   });
 }
+
+const getPrediction = function(routeId, stopId, directionId, callback) {
+
+  const config = {
+    method: 'get',
+    url: `https://api-v3.mbta.com/predictions?sort=arrival_time&filter%5Bdirection_id%5D=${directionId}&filter%5Bstop%5D=${stopId}&filter%5Broute%5D=${routeId}`,
+  };
+
+  axios(config)
+  .then((response) => {
+    callback(response.data.data)
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+
+}
+
+// getPrediction('Green-C','place-tapst', 0, (response) => {
+//   console.log(response)
+// } )
 
 module.exports = {
 
