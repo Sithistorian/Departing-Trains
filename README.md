1ca16f46f3e2b670360bf04d879240939f49850f
Tristan <tristanshawnjohnson@gmail.com>
2021-04-25 21:20:33 -0700

    Wrote a quick README

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Departing-Trains
+
 An app that gets the departing times of trains using the MBTA T Network
+
+# Installation
+
+npm install
+
+# Scripts
+
+npm run build
+npm test
+npm start
+
+# Tech Stack
+
+This app depends on Express, React, React-DOM, and Axios.
+
+# Streaming and API Key
+
+Because the MBTA API generously offers 20 requests per minute and because I was shooting for an MVP, I decided not to stream the data and really needed no API key. So you won't either.
+
+# Departure Times
+
+This App follows best practices https://www.mbta.com/developers/v3-api/best-practices suggested by MBTA. In particular, as suggested, the Arrival Time is used to display the time remaining till arrival rather than the time remaining till departure. I recognize this as being divergent from the prompt. Were this an actual client I would have clarified this. I could easily refactor to always use the departure time (when present).
+
+# Testing
+
+I'm working on learning Enzyme and Jest and hope to have coverage of the critical line soon.
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import ReactDOM from 'react-dom';
+import Enzyme, { shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-17-updated';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -10,28 +11,18 @@ import Routes from './components/Routes';
 import StopsList from './components/StopsList';
 import Directions from './components/Directions';
 import Prediction from './components/Prediction';
-import { shallow, mount } from 'enzyme';
 
 // Test Renderng
 
-describe('render components', () => {
-  it('renders the App component without crashing', () => {
-    shallow(<App/>);
+describe('The App component should be working correctly', () => {
+
+  it('should render App without crashing', () => {
+    mount(<App/>);
   });
-  it('renders the Routes component without crashing', () => {
-    shallow(<Routes/>);
-  });
-  it('renders the StopsList component without crashing', () => {
-    shallow(<StopsList/>);
-  });
-  it('renders the Directions component without crashing', () => {
-    shallow(<Directions/>);
-  });
-  it('renders the Prediction component without crashing', () => {
-    shallow(<Prediction/>);
-  });
+  it('should have Routes set in state after initialization', () => {
+    const wrapper = mount(<App/>);
+    expect(wrapper.state('routes').length).not.toEqual(0);
+  })
 })
 
-// const wrapper = shallow(<MyComponent />);
-// expect(wrapper.find(Foo)).to.have.lengthOf(3);
 
